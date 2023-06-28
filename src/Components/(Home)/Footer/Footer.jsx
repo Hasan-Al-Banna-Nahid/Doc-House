9942a0747b1fd42656b7cc6cbe391d67531bef18
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2023-06-28 23:00:58 +0600

    Updated

--- src/Components/(Home)/Footer/Footer.jsx
+++ src/Components/(Home)/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import React from "react";
 import AnimatedCursor from "react-animated-cursor";
 const Footer = () => {
@@ -20,23 +21,69 @@ const Footer = () => {
       />
       <footer className="footer p-10 bg-neutral text-neutral-content">
         <div>
-          <svg
-            width="50"
-            height="50"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fillRule="evenodd"
-            clip-rule="evenodd"
-            className="fill-current"
-          >
-            <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
-          </svg>
-          <p>
-            ACME Industries Ltd.
+          <Image
+            width={100}
+            height={100}
+            src={
+              "/Assests/d307e4100735935.Y3JvcCw4MDgsNjMyLDAsMA-removebg-preview.png"
+            }
+          />
+          <p className="font-semibold my-4 text-[18px]">
+            Doc House.
             <br />
-            Providing reliable tech since 1992
+            Providing reliable Medical Solution since 1992
           </p>
+          <button className="btn w-96 bg-[#F7A582] btn-outline">
+            Appointment
+          </button>
         </div>
+        {/* Links */}
+        <div className="grid md:grid-cols-3 gap-6">
+          <div>
+            <h2 className="text-[20px] text-slate-950 font-bold my-4">
+              Quick Links
+            </h2>
+            <ul className="text-[18px] py-4">
+              <li className="my-2">About Us</li>
+              <li className="my-2">Service</li>
+              <li className="my-2">Doctors</li>
+              <li className="my-2">Departments</li>
+              <li className="my-2">Online Payment</li>
+              <li className="my-2">Contact Us</li>
+              <li className="my-2">My Account</li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-[20px] text-slate-950 font-bold my-4">
+              Doc House Services
+            </h2>
+            <ul className="text-[18px] py-4">
+              <li className="my-2">Pediatric Clinic</li>
+              <li className="my-2">Diagnosis Clinic</li>
+              <li className="my-2">Cardiac Clinic</li>
+              <li className="my-2">Laboratory Analysis</li>
+              <li className="my-2">Gynecological Clinic</li>
+              <li className="my-2">Personal Counseling</li>
+              <li>Dental Clinic</li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="text-[20px] text-slate-950 font-bold my-4">
+              Working Hours
+            </h2>
+            <ul className="text-[16px] py-4">
+              <li className="my-2">Monday - 10 am to 7 pm</li>
+              <li className="my-2">Tuesday - 10 am to 7 pm</li>
+              <li className="my-2">Wednesday - 10 am to 7 pm</li>
+              <li className="my-2">Thursday - 10 am to 7 pm</li>
+              <li className="my-2">Friday - 10 am to 7 pm</li>
+              <li className="my-2">Saturday - 10 am to 7 pm</li>
+              <li>Sunday - 10 am to 7 pm</li>
+            </ul>
+          </div>
+        </div>
+        {/* Links */}
+
         <div>
           <span className="footer-title">Social</span>
           <div className="grid grid-flow-col gap-4">
--- src/utils/handleBookingAPI.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const handleBookingAPI = async () => {
-  const res = await fetch("http://localhost:5000/booking");
-  const data = await res.json();
-  return data;
-};
-
-export default handleBookingAPI;
