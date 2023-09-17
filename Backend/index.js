8bb5485b6febaecc4406403d3ac0bc86b5b4eb58
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2023-09-18 00:03:18 +0600

    Updated

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://doc-house.vercel.app",
+    origin: "https://doc-house.vercel.app" || "http://localhost:3000",
   })
 );
 
